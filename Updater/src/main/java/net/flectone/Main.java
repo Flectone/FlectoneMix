5a83f562bb8f4202a4ab38982ee9451ffdea6e2d
BuildTools <unconfigured@null.spigotmc.org>
2023-06-16 02:45:35 +0500

    add module updater.jar

---
+++ Updater/src/main/java/net/flectone/Main.java
@@ -0,0 +1,22 @@
+package net.flectone;
+
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        String path = args[0];
+
+        new File(path).delete();
+
+        String fileExtension = path.substring(path.lastIndexOf(".") + 1);
+
+        IOUtils.downloadFile("https://flectone.net/components/last/flectonemix." + fileExtension, path);
+
+        IOUtils.getJarProcess(path)
+                .inheritIO()
+                .start();
+
+        System.exit(0);
+    }
+}
