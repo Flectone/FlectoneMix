6e1112e7b21fd9b4c84ed70e7e8fd07fa0e44d4b
TheFaser <artem.mochalin11@gmail.com>
2023-12-26 01:21:27 +0500

    refactor

--- src/main/java/net/flectone/mix/javafx/builder/ComponentBuilder.java
+++ src/main/java/net/flectone/mix/javafx/builder/ComponentBuilder.java
@@ -2,7 +2,6 @@ package net.flectone.mix.javafx.builder;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import javafx.application.Platform;
 import javafx.collections.ObservableList;
@@ -30,87 +29,48 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 @Getter
 public class ComponentBuilder {
 
     private static final CustomThreadPool BUILD_POOL = new CustomThreadPool(1);
 
-    private final List<Component> componentList = new ArrayList<>();
-    private final HashMap<Integer, Pane> paneMap = new HashMap<>();
-
+    private final List<Component> componentList = new CopyOnWriteArrayList<>();
+    private final Map<Integer, Pane> paneMap = new ConcurrentHashMap<>();
     private final ObservableList<Node> node;
 
     private String apiUrl;
 
-    public ComponentBuilder(@NonNull ObservableList<Node> node){
+    public ComponentBuilder(@NonNull ObservableList<Node> node) {
         this.node = node;
     }
 
-
     public void build(@NonNull String type, @NonNull String version) {
-        System.out.println("build=" + Thread.currentThread());
-
         DiscordUser discordUser = FlectoneMix.getApp().getDiscordUser();
-
-        setApiUrl("https://mix.flectone.net/api/components?id=" +
-                discordUser.id() +
-                "&token=" +
-                discordUser.token() +
-                "&locale=" +
-                FlectoneMix.getApp().getConfig().getLanguage() +
-                "&type=" +
-                type
-                +"&version="
-                + version);
+        setApiUrl(String.format("https://mix.flectone.net/api/components?id=%s&token=%s&locale=%s&type=%s&version=%s",
+                discordUser.id(), discordUser.token(), FlectoneMix.getApp().getConfig().getLanguage(), type, version));
     }
 
     public void search(@NonNull String text) {
         String finalText = text.toLowerCase();
-
-        paneMap.values().forEach(pane -> pane.setVisible(false));
-        Platform.runLater(node::clear);
-
+        hideAllPanes();
         BUILD_POOL.execute(() -> componentList.forEach(component -> {
             if (paneMap.get(component.id()) == null) return;
-
-            boolean isIt = component.description().toLowerCase().contains(finalText)
-                    || component.name().toLowerCase().contains(finalText)
-                    || component.version().toLowerCase().contains(finalText)
-                    || component.type().toLowerCase().contains(finalText);
-
-            paneMap.get(component.id()).setVisible(isIt);
-
-            if (!isIt) return;
-
-            Platform.runLater(() -> {
-                if (paneMap.get(component.id()) == null) return;
-                if (node.contains(paneMap.get(component.id()))) return;
-                node.add(paneMap.get(component.id()));
-            });
+            boolean isIt = componentContainsText(component, finalText);
+            updatePaneVisibility(component, isIt);
         }));
-
         updateSelectAllCheckBox();
     }
 
     public void updateSelectAllCheckBox() {
-
-        int countVisible = (int) componentList.stream().filter(component -> {
-            Pane pane = paneMap.get(component.id());
-            return pane != null && pane.isVisible();
-        }).count();
-
-        int countSelected = (int) componentList.stream().filter(component -> {
-            Pane pane = paneMap.get(component.id());
-            return pane != null && pane.isVisible() && ((CheckBox) ((Pane) pane.getChildren().get(0)).getChildren().get(4)).isSelected();
-        }).count();
-
+        long countVisible = countVisibleComponents();
+        long countSelected = countSelectedComponents();
         boolean selected = countSelected == countVisible && countVisible != 0;
-
-        SearchPanelController searchPanelController = FlectoneMix.getApp().getPaneManager().getLoader(PaneType.SEARCH).getController();
-        searchPanelController.setSelectedAllCheckBox(selected);
+        updateSearchPanelCheckBox(selected);
     }
 
     public void clearAll() {
@@ -123,100 +83,165 @@ public class ComponentBuilder {
     }
 
     public void selectAll(boolean selected) {
-        paneMap.values().stream().filter(Node::isVisible).forEach(pane ->
-                ((CheckBox) ((Pane) pane.getChildren().get(0)).getChildren().get(4)).setSelected(selected));
+        selectAllPanes(selected);
     }
 
     public void selectAll(@NonNull List<String> filter, boolean selected) {
-        componentList.stream().filter(component -> filter.contains(component.key())).forEach(component ->
-                ((CheckBox) ((Pane) paneMap.get(component.id()).getChildren().get(0)).getChildren().get(4)).setSelected(selected));
+        selectFilteredPanes(filter, selected);
     }
 
     public void fill() {
-        if (!BUILD_POOL.getRunnableQueue().isEmpty()) {
-            BUILD_POOL.getRunnableQueue().clear();
-            paneMap.values().forEach(pane -> pane.setVisible(false));
-            Platform.runLater(node::clear);
-        }
+        clearBuildPool();
         BUILD_POOL.execute(() -> {
-            System.out.println(apiUrl);
-
-            Platform.runLater(node::clear);
-
             try {
-                paneMap.clear();
-                componentList.clear();
-                componentList.addAll(getComponentsFromApi(apiUrl));
-
-                componentList.sort(Comparator.comparing(Component::id));
-
-                componentList.forEach(component -> {
-                    FXMLLoader fxmlLoader = FlectoneMix.getApp().getPaneManager().addPane(PaneType.COMPONENT);
-                    Pane pane = FlectoneMix.getApp().getPaneManager().loadPane(PaneType.COMPONENT);
-
-                    Platform.runLater(() -> node.add(pane));
-
-                    ComponentController componentController = fxmlLoader.getController();
-                    componentController.setComponent(component);
-                    componentController.initComponent();
-
-                    if (((AppController) FlectoneMix.getApp().getPaneManager().getLoader(PaneType.APP).getController()).getSelectedTab() == PaneType.OPTIMIZATION) {
-                       componentController.getCheckBox().setSelected(!FlectoneMix.getApp().getConfig().getStringList("blacklist.optimization").contains(component.key()));
-                    }
-
-                    componentController.getCheckBox().selectedProperty().addListener(e -> updateSelectAllCheckBox());
-
-                    paneMap.put(component.id(), pane);
-                });
-
+                clearNodes();
+                loadComponents();
             } catch (IOException e) {
                 FAlert.showException(e, e.getLocalizedMessage());
             } catch (RuntimeException e) {
                 e.printStackTrace();
             }
-
             updateSelectAllCheckBox();
         });
     }
 
-    public List<Component> parseJsonResponse(String jsonResponse) {
-        List<Component> components = new ArrayList<>();
+    private void hideAllPanes() {
+        paneMap.values().forEach(pane -> pane.setVisible(false));
+        Platform.runLater(node::clear);
+    }
 
+    private boolean componentContainsText(Component component, String searchText) {
+        return component.description().toLowerCase().contains(searchText)
+                || component.name().toLowerCase().contains(searchText);
+    }
+
+    private void updatePaneVisibility(Component component, boolean isVisible) {
+        Pane pane = paneMap.get(component.id());
+        if (pane == null) return;
+
+        Platform.runLater(() -> {
+            pane.setVisible(isVisible);
+            if (isVisible && !node.contains(pane)) {
+                node.add(pane);
+            }
+        });
+    }
+
+    private long countVisibleComponents() {
+        return componentList.stream()
+                .filter(component -> isPaneVisible(component.id()))
+                .count();
+    }
+
+    private long countSelectedComponents() {
+        return componentList.stream()
+                .filter(component -> isPaneVisible(component.id()) && isCheckBoxSelected(component.id()))
+                .count();
+    }
+
+    private void updateSearchPanelCheckBox(boolean selected) {
+        SearchPanelController searchPanelController = FlectoneMix.getApp().getPaneManager().getLoader(PaneType.SEARCH).getController();
+        searchPanelController.setSelectedAllCheckBox(selected);
+    }
+
+    private void clearBuildPool() {
+        if (!BUILD_POOL.getRunnableQueue().isEmpty()) {
+            BUILD_POOL.getRunnableQueue().clear();
+            hideAllPanes();
+        }
+    }
+
+    private void clearNodes() {
+        Platform.runLater(node::clear);
+    }
+
+    private void loadComponents() throws IOException {
+        System.out.println(apiUrl);
+        List<Component> components = getComponentsFromApi(apiUrl);
+        paneMap.clear();
+        componentList.clear();
+        componentList.addAll(components);
+        componentList.sort(Comparator.comparing(Component::id));
+        addComponentsToNodes();
+    }
+
+    private void addComponentsToNodes() {
+        componentList.forEach(this::addPaneToNode);
+    }
+
+    private void addPaneToNode(Component component) {
+        FXMLLoader fxmlLoader = FlectoneMix.getApp().getPaneManager().addPane(PaneType.COMPONENT);
+        Pane pane = FlectoneMix.getApp().getPaneManager().loadPane(PaneType.COMPONENT);
+        Platform.runLater(() -> node.add(pane));
+
+        ComponentController componentController = fxmlLoader.getController();
+        componentController.setComponent(component);
+        componentController.initComponent();
+
+        updateCheckBoxForOptimization(componentController, component);
+
+        componentController.getCheckBox().selectedProperty().addListener(e -> updateSelectAllCheckBox());
+
+        paneMap.put(component.id(), pane);
+    }
+
+    private void updateCheckBoxForOptimization(ComponentController componentController, Component component) {
+        if (((AppController) FlectoneMix.getApp().getPaneManager().getLoader(PaneType.APP).getController()).getSelectedTab() != PaneType.OPTIMIZATION) return;
+
+        componentController.getCheckBox().setSelected(!FlectoneMix.getApp().getConfig().getStringList("blacklist.optimization").contains(component.key()));
+    }
+
+    private boolean isPaneVisible(int componentId) {
+        Pane pane = paneMap.get(componentId);
+        return pane != null && pane.isVisible();
+    }
+
+    private boolean isCheckBoxSelected(int componentId) {
+        Pane pane = paneMap.get(componentId);
+        return pane != null && pane.isVisible() && ((CheckBox) ((Pane) pane.getChildren().get(0)).getChildren().get(4)).isSelected();
+    }
+
+    private void selectAllPanes(boolean selected) {
+        paneMap.values().stream()
+                .filter(Node::isVisible)
+                .forEach(pane ->
+                        ((CheckBox) ((Pane) pane.getChildren().get(0)).getChildren().get(4))
+                                .setSelected(selected));
+    }
+
+    private void selectFilteredPanes(List<String> filter, boolean selected) {
+        componentList.stream()
+                .filter(component -> filter.contains(component.key()))
+                .forEach(component ->
+                        ((CheckBox) ((Pane) paneMap.get(component.id()).getChildren().get(0)).getChildren().get(4))
+                                .setSelected(selected));
+    }
+
+    private List<Component> parseJsonResponse(String jsonResponse) {
+        List<Component> components = new ArrayList<>();
         Gson gson = new Gson();
         JsonArray jsonArray = JsonParser.parseString(jsonResponse).getAsJsonArray();
-
-        for (JsonElement jsonElement : jsonArray) {
-            Component component = gson.fromJson(jsonElement, Component.class);
-            components.add(component);
-        }
-
+        jsonArray.forEach(jsonElement -> components.add(gson.fromJson(jsonElement, Component.class)));
         return components;
     }
 
-    public List<Component> getComponentsFromApi(String apiUrl) throws IOException {
+    private List<Component> getComponentsFromApi(String apiUrl) throws IOException {
         URL url = new URL(apiUrl);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
         connection.setRequestMethod("GET");
 
         int responseCode = connection.getResponseCode();
 
         if (responseCode == HttpURLConnection.HTTP_OK) {
-            BufferedReader reader = new BufferedReader(
-                    new InputStreamReader(connection.getInputStream(),
-                            StandardCharsets.UTF_8));
-            StringBuilder response = new StringBuilder();
-            String line;
-
-            while ((line = reader.readLine()) != null) {
-                response.append(line);
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8))) {
+                StringBuilder response = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+                return parseJsonResponse(response.toString());
             }
-
-            reader.close();
-
-            return parseJsonResponse(response.toString());
         }
-
         return new ArrayList<>();
     }
 
