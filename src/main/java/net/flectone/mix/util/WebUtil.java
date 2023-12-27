85083075a9d86b90d645336ccbe610d741d785d6
TheFaser <artem.mochalin11@gmail.com>
2023-12-27 22:52:32 +0500

    fix open url

--- src/main/java/net/flectone/mix/util/WebUtil.java
+++ src/main/java/net/flectone/mix/util/WebUtil.java
@@ -13,28 +13,26 @@ public class WebUtil {
 
     public static void openUrl(@NonNull String url) {
         try {
-            if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+            String os = FileManager.getOS();
+
+            if (os.contains("win")
+                    && Desktop.isDesktopSupported()
+                    && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
                 Desktop.getDesktop().browse(new URI(url));
                 return;
             }
 
-            openUrlFallback(url);
+            Runtime rt = Runtime.getRuntime();
+            if (os.contains("win")) {
+                rt.exec("rundll32 url.dll,FileProtocolHandler " + url);
+            } else if (os.contains("mac")) {
+                rt.exec("open " + url);
+            } else {
+                rt.exec("xdg-open " + url);
+            }
 
         } catch (URISyntaxException | IOException e) {
             FAlert.showException(e, e.getLocalizedMessage());
-        }
-    }
-
-    private static void openUrlFallback(String url) throws IOException {
-        Runtime rt = Runtime.getRuntime();
-        String os = FileManager.getOS();
-
-        if (os.contains("win")) {
-            rt.exec("rundll32 url.dll,FileProtocolHandler " + url);
-        } else if (os.contains("mac")) {
-            rt.exec("open " + url);
-        } else {
-            rt.exec("xdg-open " + url);
         }
     }
 }
